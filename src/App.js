67aa4e2bd38303758a9648d338d018c51570200a
Karim Elaskary <karimelaskary1@gmail.com>
2024-09-26 21:10:35 +0300

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { HomePage } from "./Pages/Home/HomePage";
-import Login from "./Pages/Auth/Login";
-import Register from "./Pages/Auth/Register";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBarLogin from "./Components/Utiltes/NaveBarLogIn";
+import { HomePage } from './Pages/Home/HomePage'
+import Login from './Pages/Auth/Login'
+import Register from './Pages/Auth/Register'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavBarLogin from './Components/Utiltes/NaveBarLogIn'
 
 function App() {
   return (
-    <div>
-      <HomePage />
-      <Login />
+    <div className='font'>
+      <NavBarLogin />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
