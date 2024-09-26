d3b7ba135a0072cb9d457bf25fd667e8e9928242
Karim Elaskary <karimelaskary1@gmail.com>
2024-09-26 20:46:01 +0300

    Routing done

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import NavBarLogin from "../../Components/Utiltes/NaveBarLogIn";
 import Slider from "../../Components/Home/Slider";
 
 export const HomePage = () => {
   return (
     <div className="font">
-      <NavBarLogin />
       <Slider />
     </div>
   );
