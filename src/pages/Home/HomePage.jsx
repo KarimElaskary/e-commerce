3487338d8b4205eb9cc43efbe14a2687a8443dd1
Karim Elaskary <karimelaskary1@gmail.com>
2024-09-26 16:59:41 +0300

    Folder Stracture done

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBarLogin from '../../components/Home/utilities/NavBarLogin'
+import Slider from '../../components/Home/Slider'
+
+const HomePage = () => {
+  return (
+    <div className='font'>
+      <NavBarLogin />
+      <Slider />
+    </div>
+  )
+}
+
+export default HomePage
