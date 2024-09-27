17aaa2aeec842ed1d8e80454763bdcd465b0dd59
Nour <nourreda276@gmail.com>
2024-09-27 13:47:27 +0300

    product card

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,11 +1,17 @@
 import Slider from "../../Components/Home/Slider";
 import HomeCategory from "../../Components/Home/HomeCategory";
+import NavBarLogin from "../../Components/Utiltes/NaveBarLogIn";
+import CardProductsContainer from "../../Components/products/CardProductsContainer";
 
-export const HomePage = () => {
+export const HomePage = ({}) => {
   return (
-    <div className="font">
+    <div className="font" style={{ minHeight: "670px" }}>
+      <NavBarLogin />
+
       <Slider />
       <HomeCategory />
+      <CardProductsContainer title="الاكثر مبيعا" btntitle="المزيد" />
+      <CardProductsContainer title=" احدث الازياء" btntitle="المزيد" />
     </div>
   );
 };
