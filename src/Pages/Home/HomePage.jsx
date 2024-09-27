4ac064f8958ff224ac5e305035a5d03b426880bd
Nour <nourreda276@gmail.com>
2024-09-27 14:16:02 +0300

    Brand Done

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -2,6 +2,8 @@ import Slider from "../../Components/Home/Slider";
 import HomeCategory from "../../Components/Home/HomeCategory";
 import NavBarLogin from "../../Components/Utiltes/NaveBarLogIn";
 import CardProductsContainer from "../../Components/products/CardProductsContainer";
+import DiscountSection from "../../Components/Home/DiscountSection";
+import BrandFeatured from "../../Components/Brand/BrandFeatured";
 
 export const HomePage = ({}) => {
   return (
@@ -11,7 +13,9 @@ export const HomePage = ({}) => {
       <Slider />
       <HomeCategory />
       <CardProductsContainer title="الاكثر مبيعا" btntitle="المزيد" />
+      <DiscountSection />
       <CardProductsContainer title=" احدث الازياء" btntitle="المزيد" />
+      <BrandFeatured title=" اشهر المركات " btntitle="المزيد" />
     </div>
   );
 };
